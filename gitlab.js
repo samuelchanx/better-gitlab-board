50fa4095d22765daf1157987e68624723f433454
Samuel Chan <cchin.samuel@gmail.com>
2019-11-01 13:49:21 +0800

    feat: support changing issue not updating sidebar

--- gitlab.js
+++ gitlab.js
@@ -1,35 +1,35 @@
 function updateIssueName(projectId, issueNumber, newName, privateToken) {
-    var data = null;
+    var data = null
 
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+    var xhr = new XMLHttpRequest()
+    xhr.withCredentials = true
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-            console.log(this.responseText);
+            console.log(this.responseText)
             // TODO: Update local issue board name
         }
-    });
+    })
 
-    xhr.open("PUT", `https://gitlab.com/api/v4/projects/${projectId}/issues/${issueNumber}?title=${encodeURIComponent(newName)}`);
-    xhr.setRequestHeader("PRIVATE-TOKEN", privateToken);
-    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.open("PUT", `https://gitlab.com/api/v4/projects/${projectId}/issues/${issueNumber}?title=${encodeURIComponent(newName)}`)
+    xhr.setRequestHeader("PRIVATE-TOKEN", privateToken)
+    xhr.setRequestHeader("cache-control", "no-cache")
 
-    xhr.send(data);
+    xhr.send(data)
 }
 
 function supportChangeName() {
     function listenForUpdate(issueNumber) {
-        document.querySelector('.new-title').onkeypress = function(event) {
+        document.querySelector('.new-title').onkeypress = function (event) {
             if (event.keyCode === 13 || event.which === 13) {
-                let gitlabToken = browser.storage.sync.get('gitlabToken');
+                let gitlabToken = browser.storage.sync.get('gitlabToken')
                 gitlabToken.then((res) => {
                     console.log(`Token retrieved: ${res.gitlabToken}`)
 
                     if (!res) throw Error('Token invalid')
                     const projectId = document.querySelector('#search_project_id').getAttribute('value')
                     const newName = document.querySelector('.new-title').innerText
-                
+
                     updateIssueName(projectId, issueNumber, newName, res.gitlabToken)
                 })
 
@@ -38,31 +38,34 @@ function supportChangeName() {
         }
     }
 
-    let targetNode = document.getElementsByClassName('right-sidebar')[0];
-    let observer = new MutationObserver(function() {
+    let targetNode = document.querySelector('.right-sidebar .issuable-header-text span')
+    let observer = new MutationObserver(function () {
         const isVisible = targetNode.style.display != 'none'
         console.log(`Target node is shown: ${isVisible}`)
-        if(isVisible) {
-            const issueName = document.querySelector('.is-active.board-card .board-card-header').textContent.trim()
-            console.log(issueName)
-            let originalTitle = document.querySelector('.right-sidebar .issuable-header-text strong')
-            originalTitle.style.display = 'none'
+        const issueName = document.querySelector('.is-active.board-card .board-card-header').textContent.trim()
+        console.log(issueName)
+        let originalTitle = document.querySelector('.right-sidebar .issuable-header-text strong')
+        originalTitle.style.display = 'none'
 
-            let newTitle = document.querySelector('div.new-title')
-            if (newTitle) {
-                newTitle.innerText = issueName
-            } else {
-                newTitle = document.createElement('div')
-                newTitle.innerHTML = `<div class="bold new-title" contenteditable="true">${issueName}</div>`
-                originalTitle.parentNode.insertBefore(newTitle, originalTitle.nextSibling)
-            }
-
-            let issueNumber = document.querySelector('.right-sidebar .issuable-header-text span').innerText.replace('#', '')
-
-            listenForUpdate(issueNumber)
+        let newTitle = document.querySelector('div.new-title')
+        if (newTitle) {
+            newTitle.innerText = issueName
+        } else {
+            newTitle = document.createElement('div')
+            newTitle.innerHTML = `<div class="bold new-title" contenteditable="true">${issueName}</div>`
+            originalTitle.parentNode.insertBefore(newTitle, originalTitle.nextSibling)
         }
-    });
-    observer.observe(targetNode, { attributes: true, childList: true });
+
+        let issueNumber = document.querySelector('.right-sidebar .issuable-header-text span').innerText.replace('#', '')
+
+        listenForUpdate(issueNumber)
+    })
+    observer.observe(targetNode, {
+        childList: true,
+        characterData: true,
+        subtree: true,
+        characterDataOldValue: true
+    })
 }
 
 try {
@@ -70,6 +73,3 @@ try {
 } catch (error) {
     console.log(error)
 }
-
-// FIXME: Just to indicate working
-document.body.style.border = "5px solid red";
